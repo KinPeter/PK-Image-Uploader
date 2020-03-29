4ce41e5df4847bfe68ad26f3d91bb371d24aa207
Peter Kin <kinpeter85@gmail.com>
2020-03-29 15:45:10 +0200

    Add multiple options to generate the hint if restrictions not set

--- src/components/image-uploader/image-uploader.wording.ts
+++ src/components/image-uploader/image-uploader.wording.ts
@@ -1,6 +1,15 @@
 export const imageUploaderWording = {
   boxText: 'Drag and drop an image here, or',
-  hint: (minW, minH, maxW, maxH, maxS) => `The image must be between ${minW}x${minH}px and ${maxW}x${maxH}px, and no larger than ${maxS}MB. Only JPG and PNG files are supported.`,
+  hint: (minW, minH, maxW, maxH, maxS) => {
+    if ((minW == 0 || minH == 0) && (maxW == 0 || maxH == 0)) {
+      return `The image must be no larger than ${maxS}MB. Only JPG and PNG files are supported.`;
+    } else if (minW == 0 || minH == 0) {
+      return `The image resolution must be smaller than ${maxW}x${maxH}px, and the file size no larger than ${maxS}MB. Only JPG and PNG files are supported.`;
+    } else if (maxW == 0 || maxH == 0) {
+      return `The image resolution must be larger than ${minW}x${minH}px, and the file size no larger than ${maxS}MB. Only JPG and PNG files are supported.`;
+    }
+    return `The image must be between ${minW}x${minH}px and ${maxW}x${maxH}px, and no larger than ${maxS}MB. Only JPG and PNG files are supported.`;
+  },
   browseButton: 'BROWSE',
   removeButton: 'REMOVE',
   smallError: (minW, minH) => `The selected image is too small. It must be bigger than ${minW}x${minH}px.`,
